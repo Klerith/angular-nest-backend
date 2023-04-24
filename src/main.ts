0e8d21eecdf992362e1d81635ded51f47ef326b8
Fernando Herrera <fernando.herrera85@gmail.com>
2023-04-24 14:59:59 -0400

    CORS Added

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
